6098e3a3f295ee109e2fb518d2c54fcf1a48589b
Quillenator <32397666+Quillenator@users.noreply.github.com>
2026-06-13 14:32:06 -0400

    Added Spawn egg for rat_wolf Added some TODOs to fix a bug in AttackBlockGoal Added english representation for egg name in creative menu

--- src/main/java/org/ratden/skavenblight/item/ModItems.java
+++ src/main/java/org/ratden/skavenblight/item/ModItems.java
@@ -1,9 +1,12 @@
 package org.ratden.skavenblight.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SpawnEggItem;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.ratden.skavenblight.ModEntities;
 import org.ratden.skavenblight.Skavenblight;
 import org.ratden.skavenblight.item.custom.RatJuiceItem;
 
@@ -11,11 +14,13 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Skavenblight.MODID);
 
     public static final DeferredItem<Item> RAW_WARPSTONE = ITEMS.register("raw_warpstone",
-            () -> new Item(new Item.Properties()));
+        () -> new Item(new Item.Properties()));
 
     public static final DeferredItem<Item> RAT_JUICE = ITEMS.register("rat_juice",
         () -> new RatJuiceItem(new Item.Properties().food(ModFoodProperties.RAT_JUICE)));
 
+    public static final DeferredHolder<Item, SpawnEggItem> RAT_WOLF_SPAWN_EGG = ITEMS.register("rat_wolf_spawn_egg",
+        () -> new SpawnEggItem(ModEntities.RAT_WOLF.get(), 0x4A3728, 0x8B000, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
