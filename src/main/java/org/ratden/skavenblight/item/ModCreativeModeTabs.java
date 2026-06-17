cdd2ac608b8038beed46573f583500f4d3b1dbe7
bobthe13th <bobthe13th@gmail.com>
2026-06-16 21:37:36 -0600

    added Small Spawn Tunnel

--- src/main/java/org/ratden/skavenblight/item/ModCreativeModeTabs.java
+++ src/main/java/org/ratden/skavenblight/item/ModCreativeModeTabs.java
@@ -33,6 +33,7 @@ public class ModCreativeModeTabs {
                         output.accept(ModBlocks.WARPSTONE_DOOR);
                         output.accept(ModBlocks.WARPSTONE_TRAPDOOR);
 
+                        output.accept(ModBlocks.SPAWN_TUNNEL_SMALL);
 
                         output.accept(ModItems.RAW_WARPSTONE);
                         output.accept(ModItems.FUSED_WARPSTONE);
